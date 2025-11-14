489b5436a5e0656b1d88166bd9aa2ff0e2846f67
Artiom Poluyanovich <apoluyanovich@gmail.com>
2025-11-14 13:15:18 +0100

    AU-Feature(initial): created ArtemPoluyanovichTechTask project

---
+++ ArtemPoluyanovichTechTask/ArtemPoluyanovichTechTask/ArtemPoluyanovichTechTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtemPoluyanovichTechTaskApp.swift
+//  ArtemPoluyanovichTechTask
+//
+//  Created by Artiom Poluyanovich on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtemPoluyanovichTechTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
