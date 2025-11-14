69ce10e6f4a812c713f66276b9ea66219ddeb7f8
Artiom Poluyanovich <apoluyanovich@gmail.com>
2025-11-14 13:54:59 +0100

    AU-Feature(initial): added clean architecture structure

--- ArtemPoluyanovichTechTask/ArtemPoluyanovichTechTask/App/ArtemPoluyanovichTechTaskApp.swift
+++ ArtemPoluyanovichTechTask/ArtemPoluyanovichTechTask/App/ArtemPoluyanovichTechTaskApp.swift
@@ -10,8 +10,6 @@ import SwiftUI
 @main
 struct ArtemPoluyanovichTechTaskApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup { }
     }
 }
